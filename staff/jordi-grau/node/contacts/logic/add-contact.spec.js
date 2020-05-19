2a9ea1dcdd5a941d9b9c65baf0d1b5d8b27cf898
jordigraup <jordigptab@gmail.com>
2020-05-20 00:13:04 +0200

    add list-contact.spec

--- staff/jordi-grau/node/contacts/logic/add-contact.spec.js
+++ staff/jordi-grau/node/contacts/logic/add-contact.spec.js
@@ -1,6 +1,8 @@
 const assert = require('assert')
 const addContact = require('./add-contact')
 const { random } = Math
+const fs = require('fs')
+const path = require('path')
 
 {
     const name = `name-${random()}`
@@ -8,10 +10,26 @@ const { random } = Math
     const email = `e-${random()}@mail.com`
 
     addContact({ name, surname, email }, (error, id) => {
-        assert(error === null)
+        assert(!error)
 
         assert(typeof id === 'string')
-        // TODO check that file <id>.json exists in data folder
-        // TODO read file and check that it contains the name, surname and email
+
+        // fs.readFile(path.join(__dirname, '..', 'data', `${id}.json`), (error, content) => {
+        fs.readFile(path.join(__dirname, '..', 'data', `${id}.json`), 'utf8', (error, content) => {
+            assert(!error)
+
+            assert(content)
+
+            // const json = content.toString()
+
+            // const contact = JSON.parse(json)
+            const contact = JSON.parse(content)
+
+            assert.equal(contact.name, name)
+            assert.equal(contact.surname, surname)
+            assert.equal(contact.email, email)
+
+            // TODO clean data
+        })
     })
 }
--- staff/jordi-grau/node/contacts/logic/list-contacts.spec.js
+++ staff/jordi-grau/node/contacts/logic/list-contacts.spec.js
@@ -1,1 +1,25 @@
 // TODO check list contacts returns an array of valid contacts
+// STEPS
+// 1. clean data => USE fs.readdir, fs.unlink
+// 2. create contacts by hand => USE fs.writeFile
+// 3. launch logic
+// 4. validate results against data
+// 5. clean data ...
+
+const fs = require('fs')
+const assert = require('assert')
+const listContacts = require('./list-contacts')
+const path = require('path')
+
+
+fs.unlink('./list-contacts.js', (error)=>{
+    if(error) throw new Error(error)
+
+    console.log('list-contacts cleared....')
+})
+
+fs.readdir('./list.contacts', (error, files =>{
+    if(error) throw new Error(error) 
+}))
+
+
